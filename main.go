b9fb274b86f04d77a558dee76940ed7eed5e51f9
Captaintobi <tinh572@gmail.com>
2017-03-02 21:35:07 -0800

    Login system

--- main.go
+++ main.go
@@ -1,84 +1,105 @@
 package main
 
-import (
-	"crypto/sha1"
-	"fmt"
-	"html/template"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
-	"github.com/satori/go.uuid"
+import "golang.org/x/crypto/bcrypt"
+
+import (
+	"fmt"
+	"net/http"
 )
 
-var tpl *template.Template
+var db *sql.DB
+var err error
 
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
-}
-func main() {
-	http.HandleFunc("/", index)
-	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
-	http.ListenAndServe(":8080", nil)
-
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	c := getCookie(w, r)
-	if r.Method == http.MethodPost {
-		mf, fh, err := r.FormFile("nf")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer mf.Close()
-
-		ext := strings.Split(fh.Filename, ".")[1]
-		h := sha1.New()
-		io.Copy(h, mf)
-		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
-
-		wd, err := os.Getwd()
-		if err != nil {
-			fmt.Println(err)
-		}
-		path := filepath.Join(wd, "public", "pics", fname)
-		nf, err := os.Create(path)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer nf.Close()
-
-		mf.Seek(0, 0)
-		io.Copy(nf, mf)
-
-		c = appendValue(w, c, fname)
+func signupPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "signup.html")
+		return
 	}
-	xs := strings.Split(c.Value, "|")
-	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 
-}
-func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
-	s := c.Value
-	if !strings.Contains(s, fname) {
-		s += "|" + fname
+	fmt.Println("hello")
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var user string
+
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+	fmt.Fprintln(res, err)
+
+	switch {
+	case err == sql.ErrNoRows:
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO users(username, password) VALUES(?, ?)", username, hashedPassword)
+		if err != nil {
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		}
+
+		res.Write([]byte("User created!"))
+		return
+	case err != nil:
+		http.Error(res, "Server error, unable to create your account.", 500)
+		return
+	default:
+		http.Redirect(res, req, "/", 301)
 	}
-	c.Value = s
-	http.SetCookie(w, c)
-	return c
-
 }
-func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
-	c, err := r.Cookie("session")
+
+func loginPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.ServeFile(res, req, "login.html")
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var databaseUsername string
+	var databasePassword string
+
+	err := db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&databaseUsername, &databasePassword)
+
 	if err != nil {
-		sID := uuid.NewV4()
-		c = &http.Cookie{
-			Name:  "sessios",
-			Value: sID.String(),
-		}
-		http.SetCookie(w, c)
+		http.Redirect(res, req, "/login", 301)
+		return
 	}
-	return c
 
+	err = bcrypt.CompareHashAndPassword([]byte(databasePassword), []byte(password))
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		return
+	}
+
+	res.Write([]byte("Hello" + databaseUsername))
+
+}
+
+func homePage(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "index.html")
+}
+
+func main() {
+
+	db, err = sql.Open("mysql", "root:1234@/user")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.HandleFunc("/signup", signupPage)
+	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
 }
