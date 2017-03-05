b112183778b5c7a4c197c3bf2cb8c0edc75b7281
Captaintobi <tinh572@gmail.com>
2017-03-04 22:13:22 -0800

    Added file input and to put it in the public/pics

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql"
-
-import "golang.org/x/crypto/bcrypt"
-
 import (
+	"crypto/sha1"
+	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var db *sql.DB
@@ -19,14 +23,43 @@ func signupPage(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	fmt.Println("hello")
 	username := req.FormValue("username")
 	password := req.FormValue("password")
+	//grab file name
+	if req.Method == http.MethodPost {
+		// mf == multipart file fh== *multipart header
+		mf, fh, err := req.FormFile("fileName")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+
+		fname := fmt.Sprintf("%x", h.Sum(nil))[:10] + "." + ext
+		fmt.Println(fname)
+
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+
+	}
 
 	var user string
 
 	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
-	fmt.Fprintln(res, err)
 
 	switch {
 	case err == sql.ErrNoRows:
@@ -77,7 +110,7 @@ func loginPage(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.Write([]byte("Hello" + databaseUsername))
+	res.Write([]byte("Hello " + databasePassword))
 
 }
 
@@ -101,5 +134,5 @@ func main() {
 	http.HandleFunc("/signup", signupPage)
 	http.HandleFunc("/login", loginPage)
 	http.HandleFunc("/", homePage)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":1027", nil)
 }
